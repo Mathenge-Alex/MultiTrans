46947267b4261e8dd22a3aeff18ad2e25fe4d2a5
Mathenge-Alex <arxmaina@gmail.com>
2022-06-27 11:20:43 +0300

    Feat: Add Bus Booking Model class

--- myapp/models.py
+++ myapp/models.py
@@ -27,3 +27,25 @@ class User(models.Model):
     def __str__(self):
         return self.email
 
+
+class Book(models.Model):
+    BOOKED = 'B'
+    CANCELLED = 'C'
+
+    TICKET_STATUSES = ((BOOKED, 'Booked'),
+                        (CANCELLED, 'Cancelled'),)
+    email = models.EmailField()
+    name = models.CharField(max_length=30)
+    userid =models.DecimalField(decimal_places=0, max_digits=2)
+    busid=models.DecimalField(decimal_places=0, max_digits=2)
+    bus_name = models.CharField(max_length=30)
+    source = models.CharField(max_length=30)
+    dest = models.CharField(max_length=30)
+    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(choices=TICKET_STATUSES, default=BOOKED, max_length=2)
+
+    def __str__(self):
+        return self.email
