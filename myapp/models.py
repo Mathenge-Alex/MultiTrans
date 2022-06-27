e2de7d5ae890e76b559eb6099935d9a4f13bf20d
Mathenge-Alex <arxmaina@gmail.com>
2022-06-27 11:19:29 +0300

    Feat: Add Bus Model

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 # Create your models here.
 
+class Bus(models.Model):
+    bus_name = models.CharField(max_length=30)
+    source = models.CharField(max_length=30)
+    dest = models.CharField(max_length=30)
+    nos = models.DecimalField(decimal_places=0, max_digits=2)
+    rem = models.DecimalField(decimal_places=0, max_digits=2)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    date = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.bus_name
+
