ad5a25949980bd60e9ae5d00fa47f5c8d1f0c929
Mathenge-Alex <arxmaina@gmail.com>
2022-06-27 11:23:11 +0300

    Feat: Add user account login form

--- myapp/forms.py
+++ myapp/forms.py
@@ -35,3 +35,21 @@ class UserRegisterForm(forms.ModelForm):
         return super(UserRegisterForm, self).clean(*args, **kwargs)
 
 
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError('This user has not been authenticated')
+            if not user.check_password(password):
+                raise forms.ValidationError('You Have Entered an Incorrect password')
+            if not user.is_active:
+                raise forms.ValidationError('This user is inactive')
+        return super(UserLoginForm, self).clean(*args, **kwargs)
+
