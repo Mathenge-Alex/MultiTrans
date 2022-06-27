8ae51cb79e74f070a8eddd97955ff6118ccbf9d5
Mathenge-Alex <arxmaina@gmail.com>
2022-06-27 11:27:17 +0300

    Feat: Add booking bus page view

--- myapp/views.py
+++ myapp/views.py
@@ -35,3 +35,41 @@ def findbus(request):
     else:
         return render(request, 'myapp/findbus.html')
 
+
+@login_required(login_url='signin')
+def bookings(request):
+    context = {}
+    if request.method == 'POST':
+        id_r = request.POST.get('bus_id')
+        seats_r = int(request.POST.get('no_seats'))
+        bus = Bus.objects.get(id=id_r)
+        if bus:
+            if bus.rem > int(seats_r):
+                name_r = bus.bus_name
+                cost = int(seats_r) * bus.price
+                source_r = bus.source
+                dest_r = bus.dest
+                nos_r = Decimal(bus.nos)
+                price_r = bus.price
+                date_r = bus.date
+                time_r = bus.time
+                username_r = request.user.username
+                email_r = request.user.email
+                userid_r = request.user.id
+                rem_r = bus.rem - seats_r
+                Bus.objects.filter(id=id_r).update(rem=rem_r)
+                book = Book.objects.create(name=username_r, email=email_r, userid=userid_r, bus_name=name_r,
+                                            source=source_r, busid=id_r,
+                                            dest=dest_r, price=price_r, nos=seats_r, date=date_r, time=time_r,
+                                            status='BOOKED')
+                print('------------book id-----------', book.id)
+                # book.save()
+                return render(request, 'myapp/bookings.html', locals())
+            else:
+                context["error"] = "Sorry, we have les seats available"
+                return render(request, 'myapp/findbus.html', context)
+
+    else:
+        return render(request, 'myapp/findbus.html')
+
+
