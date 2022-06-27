83378bb16b014ca08f350c36d41ed735874242b9
Mathenge-Alex <arxmaina@gmail.com>
2022-06-27 11:20:15 +0300

    Feat: Add user class Model

--- myapp/models.py
+++ myapp/models.py
@@ -17,3 +17,13 @@ class Bus(models.Model):
     def __str__(self):
         return self.bus_name
 
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField()
+    name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.email
+
